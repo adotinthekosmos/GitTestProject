bcac564451646544115dec0ab142796306859d69
khoapa <khoateamer@gmail.com>
2017-12-03 17:25:15 +0700

    Added a job

--- app/src/main/java/com/example/khoapa/gittestproject/MainActivity.kt
+++ app/src/main/java/com/example/khoapa/gittestproject/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.khoapa.gittestproject
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var occupation = "developer"
+
     }
 }
